73aa7ab8a0c066b102bf56f0acf8d86eb806281f
IsabelaAR <37556162+IsabelaAR@users.noreply.github.com>
2025-11-02 04:22:17 +0000

    manipulação2

--- import os.py
+++ import os.py
@@ -3,9 +3,18 @@ import os
 arquivo1 = open("dados1.txt", 'w', encoding='utf-8') #"enconding" e pra n bugar as palavras acentos
 print(os.path.abspath(arquivo1.name))
 
+print("Nome do arquivo:", arquivo1.name)
+print("Modo de abertura:",arquivo1.mode)
+print("Arquivo está fechado?", arquivo1.closed)
+
 arquivo1.write("Olá, mundo!!!")
 
-print(os.path.relpath(arquivo1.name))
-print(arquivo1)
-
 arquivo1.close()
+
+print("Arquivo está fechado agora? ", arquivo1.closed)
+
+relpath = os.path.relpath('dados1.txt')
+abspath = os.path.abspath('dados1.txt')
+
+print("Caminho relativo:", relpath)
+print("Caminho absoluto:", abspath)
