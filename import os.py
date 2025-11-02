6bfaa6263b91bb8448cdc360a07a9c6503839906
IsabelaAR <37556162+IsabelaAR@users.noreply.github.com>
2025-11-02 04:03:34 +0000

    manipulação1.3

--- import os.py
+++ import os.py
@@ -1,6 +1,11 @@
 import os
 
-arquivo1 = open("dados1.txt", 'w')
+arquivo1 = open("dados1.txt", 'w', encoding='utf-8') #"enconding" e pra n bugar as palavras acentos
 print(os.path.abspath(arquivo1.name))
 
 arquivo1.write("Olá, mundo!!!")
+
+print(os.path.relpath(arquivo1.name))
+print(arquivo1)
+
+arquivo1.close()
